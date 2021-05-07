66a2ae257c544805a4ab6933b4fe8689023744f8
João Victor da Silva <jv782063@gmail.com>
2021-05-07 15:34:50 -0300

    add react navigation

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
