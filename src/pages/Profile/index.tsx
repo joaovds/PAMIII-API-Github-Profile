2c339b967fbe48f9f0ccecb4f4a622054c87aab5
João Victor da Silva <jv782063@gmail.com>
2021-05-07 17:50:37 -0300

    create profile page

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,13 +1,47 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, Text, View, ScrollView } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import styles from './styles';
 
 const Profile: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Image
+          style={styles.user_image}
+          source={{
+            uri: 'https://avatars.githubusercontent.com/u/50719694?v=4',
+          }}
+        />
+
+        <Text style={styles.name}>João Victor</Text>
+
+        <View style={styles.hr} />
+
+        <Text style={styles.infoTitle}>Infos:</Text>
+
+        <ScrollView>
+          <Text style={styles.infos}>Username: joaovds</Text>
+          <Text style={styles.infos}>
+            Bio: Cursando Técnico em Desenvolvimento de Sistemas
+          </Text>
+          <Text style={styles.infos}>Followers: 12</Text>
+          <Text style={styles.infos}>Following: 25</Text>
+          <Text style={styles.infos}>Company: Etec de Peruíbe</Text>
+          <Text style={styles.infos}>Public Repos: 23</Text>
+          <Text style={styles.infos}>Location: Peruíbe , SP - Brasil</Text>
+
+          <RectButton style={styles.button}>
+            <Text style={styles.buttonText}>Ver Repositórios</Text>
+          </RectButton>
+          <RectButton style={styles.button}>
+            <Text style={styles.buttonText}>Ver Organizações</Text>
+          </RectButton>
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 };
 
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -5,6 +5,66 @@ export default StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 24,
-    paddingTop: Constants.statusBarHeight + 20,
+    paddingTop: 15,
+  },
+
+  user_image: {
+    alignSelf: 'center',
+    width: 180,
+    height: 180,
+    resizeMode: 'cover',
+    borderRadius: 100,
+    borderWidth: 1,
+    borderColor: '#FFFBEB'
+  },
+
+  name: {
+    alignSelf: 'center',
+    color: '#FFFFFF',
+    fontSize: 20,
+    maxWidth: 260,
+    lineHeight: 24,
+    marginVertical: 10,
+  },
+
+  hr: {
+    borderBottomColor: '#2e2e2e',
+    borderBottomWidth: 1,
+    marginVertical: 2
+  },
+
+  infoTitle: {
+    color: '#999999',
+    fontSize: 18,
+    maxWidth: 260,
+    lineHeight: 20,
+    marginTop: 20,
+    marginBottom: 5
+  },
+
+  infos: {
+    color: '#cccccc',
+    fontSize: 15,
+    maxWidth: 260,
+    lineHeight: 20,
+    marginVertical: 12,
+  },
+
+  button: {
+    backgroundColor: '#141414',
+    height: 60,
+    flexDirection: 'row',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+
+  buttonText: {
+    justifyContent: 'center',
+    textAlign: 'center',
+    color: '#FFF',
+    fontSize: 16,
   },
 })
