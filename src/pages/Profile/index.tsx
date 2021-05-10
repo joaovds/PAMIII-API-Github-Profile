ea1ff6617267cc5e6a874992c7c1981329ebdc6f
João Victor da Silva <jv782063@gmail.com>
2021-05-10 15:17:58 -0300

    consuming api

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRoute } from '@react-navigation/core';
 import { Image, Text, View, ScrollView } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import styles from './styles';
+import api from '../../services/api';
 
 interface IParams {
   username: string;
 }
 
+interface IUserInfo {
+  login?: string;
+  name?: string;
+  bio?: string;
+  followers?: number;
+  following?: number;
+  company?: string;
+  public_repos?: number;
+  location?: string;
+  repos_url?: string;
+  organizations_url?: string;
+  html_url?: string;
+  avatar_url?: string;
+
+  message?: string;
+}
+
 const Profile: React.FC = () => {
   const route = useRoute();
+
+  const [userInfo, setUserInfo] = useState<IUserInfo>();
 
   const routeParams = route.params as IParams;
   const { username } = routeParams;
 
-  console.log(username);
+  useEffect(() => {
+    api.get(username).then((response) => setUserInfo(response.data));
+  }, []);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -24,26 +46,26 @@ const Profile: React.FC = () => {
         <Image
           style={styles.user_image}
           source={{
-            uri: 'https://avatars.githubusercontent.com/u/50719694?v=4',
+            uri: userInfo?.avatar_url,
           }}
         />
 
-        <Text style={styles.name}>João Victor</Text>
+        <Text style={styles.name}>{userInfo?.name}</Text>
 
         <View style={styles.hr} />
 
         <Text style={styles.infoTitle}>Infos:</Text>
 
         <ScrollView>
-          <Text style={styles.infos}>Username: joaovds</Text>
+          <Text style={styles.infos}>Username: {userInfo?.login}</Text>
+          <Text style={styles.infos}>Bio: {userInfo?.bio}</Text>
+          <Text style={styles.infos}>Followers: {userInfo?.followers}</Text>
+          <Text style={styles.infos}>Following: {userInfo?.following}</Text>
+          <Text style={styles.infos}>Company: {userInfo?.company}</Text>
           <Text style={styles.infos}>
-            Bio: Cursando Técnico em Desenvolvimento de Sistemas
+            Public Repos: {userInfo?.public_repos}
           </Text>
-          <Text style={styles.infos}>Followers: 12</Text>
-          <Text style={styles.infos}>Following: 25</Text>
-          <Text style={styles.infos}>Company: Etec de Peruíbe</Text>
-          <Text style={styles.infos}>Public Repos: 23</Text>
-          <Text style={styles.infos}>Location: Peruíbe , SP - Brasil</Text>
+          <Text style={styles.infos}>Location: {userInfo?.location}</Text>
 
           <RectButton style={styles.button}>
             <Text style={styles.buttonText}>Ver Repositórios</Text>
